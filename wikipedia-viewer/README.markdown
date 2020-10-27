7d717359523c563b154cf7177d8225d1ee4637f5
jdegand <70610011+jdegand@users.noreply.github.com>
2020-10-27 15:54:41 -0400

    Add files via upload

---
+++ wikipedia-viewer/README.markdown
@@ -0,0 +1,7 @@
+# Wikipedia Viewer
+
+Simple project built for FreeCodeCamp
+
+Random button grabs a random Wikipedia page.  
+
+Use the input box to grab 10 links that contain your search term.
---
+++ wikipedia-viewer/src/script.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', searchWikipedia);
+
+function searchWikipedia(e) {
+  e.preventDefault();
+
+  const input = document.querySelector('#input').value;
+
+fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&origin=*&srsearch=${input}&format=json`)
+  .then(response => response.json())
+  .then(data => showData(data.query.search))
+}
+
+function showData(data){
+  const output = document.getElementById('output');
+
+  output.textContent = '';
+
+  data.forEach((title)=> {
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <div>
+        <ul>
+          <li>${title.title}</li>
+          <li style="list-style-type:none">${title.snippet}</li>
+        </ul>
+      </div>
+    `;
+
+    output.appendChild(div);
+
+  })
+}
